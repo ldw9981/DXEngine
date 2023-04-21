ccf21158a1d573f897cbb99f2b9dd8bbb0753ae8
LeeDongWon <ldw9981@gmail.com>
2023-04-21 14:53:37 +0900

    initial

---
+++ DXEngine18_EngineUpdate/Engine.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "DXApp.h"
+
+class Engine : public DXApp
+{
+public:
+	Engine(HINSTANCE hinstance);
+	~Engine();
+
+	bool Init() override;
+	void Update(double deltaTime) override;
+	void Render() override;
+	void DrawMesh();
+
+	void RotateObject(double deltaTime);			// 물체 회전 함수.
+};
